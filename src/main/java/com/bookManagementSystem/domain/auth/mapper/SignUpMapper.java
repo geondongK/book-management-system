b7236bc69a92cdde37db333d44fd02fd4be28591
GeonDong <ehd563@naver.com>
2023-12-10 23:21:18 +0900

    Add: 회원가입 예외처리 기능 추가  - 이메일 중복 체크  - 전화번호 중복 체크

--- src/main/java/com/bookManagementSystem/domain/auth/mapper/SignUpMapper.java
+++ src/main/java/com/bookManagementSystem/domain/auth/mapper/SignUpMapper.java
@@ -5,5 +5,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SignUpMapper {
+    // 회원가입
     void signUp(Users dto);
+
+    // 이메일 중복 체크
+    boolean existsByEmail(String email);
+
+    // 이름 중복 체크
+    // boolean existsByName(String name);
+
+    // 전화번호 중복 체크
+    boolean existsByPhoneNumber(String phoneNumber);
 }
