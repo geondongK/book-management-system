d78a4f98bf340c45f9e30dffc3cd30417a0a6631
GeonDong <ehd563@naver.com>
2023-12-13 18:06:50 +0900

    Add: 전역 예외 처리  - 유효성 검사 에러 핸들링 추가  - 유저, 도서 에러 핸들링 추가

--- src/main/java/com/bookManagementSystem/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bookManagementSystem/global/exception/GlobalExceptionHandler.java
@@ -1,17 +1,27 @@
 package com.bookManagementSystem.global.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.sql.SQLException;
+import javax.xml.crypto.MarshalException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @Slf4j
 @RestControllerAdvice // 전역 예외 처리
@@ -23,4 +33,37 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(new GlobalExceptionResponse(e.getErrorCode()));
     }
+
+    /* valid 검증 에러
+      @ExceptionHandler에 이미 MethodArgumentNotValidException이 구현되어있기
+      때문에 GlobalExceptionHandler에서 동일한 예외 처리를 하게 되면, Ambiguous(모호성) 문제가 발생
+      spring boot 3.0 이상 HttpStatus status -> HttpStatusCode status
+    */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers,
+            HttpStatusCode status, WebRequest request) {
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+        });
+        ObjectError objectError = ex.getBindingResult().getAllErrors().stream().findFirst().get();
+        String errorMsg = "[errors] field : " + ((FieldError) objectError).getField() + ", errorMessage: " + objectError.getDefaultMessage();
+        return ResponseEntity.badRequest().body(errorMsg);
+
+//        Map<String, List<String>> body = new HashMap<>();
+//
+//        List<String> errors = ex.getBindingResult()
+//                .getFieldErrors()
+//                .stream()
+//                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                .collect(Collectors.toList());
+//
+//        body.put("errors", errors);
+//
+//        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 }
+
+
--- src/main/java/com/bookManagementSystem/global/exception/GlobalExceptionResponseCode.java
+++ src/main/java/com/bookManagementSystem/global/exception/GlobalExceptionResponseCode.java
@@ -7,10 +7,17 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum GlobalExceptionResponseCode {
+    /* 전역 */
+    VALIDATION_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "유효성 검증에 실패하였습니다"),
+    
     /* 유저 */
     EMAIL_EXISTS_FOUND(HttpStatus.CONFLICT, "중복된 이메일입니다"),
     NAME_EXISTS_FOUND(HttpStatus.CONFLICT, "중복된 이름입니다"),
-    PHONE_NUMBER_EXISTS_FOUND(HttpStatus.CONFLICT, "가입된 전화번호입니다");
+    PHONE_NUMBER_EXISTS_FOUND(HttpStatus.CONFLICT, "가입된 전화번호입니다"),
+    USER_NOT_FOUND(HttpStatus.CONFLICT, "이메일 또는 비밀번호가 틀렸습니다"),
+
+    /* 도서 */
+    BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "책 정보가 없습니다");
 
     private final HttpStatus httpStatus;
     private final String detailMessage;
